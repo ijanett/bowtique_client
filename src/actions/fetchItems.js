e41a936a4bd03961bfbe272ef732cc561746f7d7
Irma Mejia <ijanettmejia@outlook.com>
2019-10-13 16:07:46 -0400

    Add action to fetch items

---
+++ src/actions/fetchItems.js
@@ -0,0 +1,13 @@
+const ITEMS_URL = "http://localhost:3000/items"
+
+export const fetchItems = () => {
+    console.log("INSIDE");
+    return (dispatch) => {
+        console.log("FETCHING");
+        
+        dispatch({type: 'LOADING_ITEMS'})
+        fetch(ITEMS_URL)
+            .then(res => res.json())
+            .then(items => dispatch({ type: 'RENDER_ITEMS', items: items.data }))
+    }
+}
