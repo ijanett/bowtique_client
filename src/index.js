0645e5bcf27ad839c3d6d6368b9cd4f7b1d66ffd
Irma Mejia <ijanettmejia@outlook.com>
2019-10-13 16:05:01 -0400

    Add store to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
+import thunk from 'redux-thunk';
+import itemsReducer from './reducers/itemsReducer.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(itemsReducer, applyMiddleware(thunk))
 
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
