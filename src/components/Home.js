d8867582941f5c6d99035d2fc434983031a49a45
Irma Mejia <ijanettmejia@outlook.com>
2019-10-16 23:41:15 -0400

    Add class to home page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ export default class Home extends React.Component {
             <div className="thumbnail text-center">
                 <img src={image} className="img-fluid" alt="bowtique" />
                 <div className="caption">
-                    <img src={logo} alt="" />
+                    <img className="img-fluid" src={logo} alt="" />
                     <br />
-                    <button onClick={() => {this.props.history.replace('/bowties')}} type="button" className="btn-arrow-right">SHOP NOW ></button>
+                    <button onClick={() => {this.props.history.replace('/bowties')}} type="button">SHOP NOW ></button>
                 </div>
             </div>
         )
