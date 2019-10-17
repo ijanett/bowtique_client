01dcd269d5f25f40d8edcb2c9693521348dd9366
Irma Mejia <ijanettmejia@outlook.com>
2019-10-16 22:31:21 -0400

    Add navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import brandName from '../brandName.png';
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <NavLink to="/" className="nav-link">Home</NavLink>
-                <NavLink to="/bowties" className="nav-link">Shop</NavLink>
-            </nav> 
+        <div className="navbar">
+            <NavLink to="/" className="navbar-logo" activeClassName="chosen"><img src={brandName} alt=""/></NavLink>
+            <div className="navbar-list">
+                <NavLink to="/" activeClassName="chosen">Home</NavLink>
+                <NavLink to="/login" activeClassName="chosen">Login</NavLink>
+                
+                <NavLink to="/bowties" activeClassName="chosen">Shop</NavLink>
+            </div> 
         </div>
     )
 }
