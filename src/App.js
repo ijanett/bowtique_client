d55912d2ee7fb8db8f170b55edfe354ad37e95c0
Irma Mejia <ijanettmejia@outlook.com>
2019-10-16 22:35:34 -0400

    Create login and user routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Header from './components/Header';
 import Items from './components/Items';
+import User from './components/User';
+import Login from './components/Login';
 
 
-export default class App extends Component {
+
+class App extends Component {
+  
+  
   render() {
+    console.log(this.props);
     return (
       <Router>
       <Navbar />
+      <Header />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
           <Route path="/bowties" component={Items} />
+          <Route path="/account" component={User} />
         </Switch>
       </Router>
     )
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    user: state.userReducer.user
+  }
+}
+
+export default connect(mapStateToProps)(App)
 
 
 //  {/* <div className="App">
