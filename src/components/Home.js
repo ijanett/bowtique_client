653466802c5393509234e6f1ca9291ae339a1a07
Irma Mejia <ijanettmejia@outlook.com>
2019-10-16 22:33:18 -0400

    Create home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import image from '../bowtiqueHomeImg.jpg'
+import logo from '../homeLogo.png'
 
 export default class Home extends React.Component {
     render() {
-        return <h1>Home</h1>
+        return (
+            <div className="thumbnail text-center">
+                <img src={image} className="img-fluid" alt="bowtique" />
+                <div className="caption">
+                    <img src={logo} alt="" />
+                    <br />
+                    <button onClick={() => {this.props.history.replace('/bowties')}} type="button" className="btn-arrow-right">SHOP NOW ></button>
+                </div>
+            </div>
+        )
     }
 }
