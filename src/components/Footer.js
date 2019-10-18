5e9b8bd43cecaa866e2307ab5b7176aeecdc01c5
Irma Mejia <ijanettmejia@outlook.com>
2019-10-18 03:02:32 -0400

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="page-footer font-small">
+        <div className="footer-copyright text-center py-2">© 2019 BOWTIQUE</div>
+    </footer>
+)
+
+export default Footer;
