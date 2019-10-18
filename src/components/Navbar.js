cd30131a3cf639764e8ce1359f572f88fd4e03d6
Irma Mejia <ijanettmejia@outlook.com>
2019-10-18 03:04:23 -0400

    Connect navbar component to store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import brandName from '../brandName.png';
+import { connect } from 'react-redux';
 
-const Navbar = () => {
-    return (
-        <div className="navbar">
-            <NavLink to="/" className="navbar-logo" activeClassName="chosen"><img src={brandName} alt=""/></NavLink>
-            <div className="navbar-list">
-                <NavLink to="/" activeClassName="chosen">Home</NavLink>
-                <NavLink to="/login" activeClassName="chosen">Login</NavLink>
-                
-                <NavLink to="/bowties" activeClassName="chosen">Shop</NavLink>
-            </div> 
-        </div>
-    )
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="navbar">
+                <NavLink to="/" className="navbar-logo"><img src={brandName} alt=""/></NavLink>
+                <div className="navbar-list">
+                    <NavLink to="/" activeClassName="chosen">Home</NavLink>
+                    <NavLink to="/bowties" activeClassName="chosen">Shop</NavLink>
+                    
+                    {this.props.currentUser !== null ? <NavLink to="/account" activeClassName="chosen">My Account</NavLink> : <NavLink to="/login" activeClassName="chosen">Login</NavLink> }
+                    
+                    <NavLink to="/carts/new" className="cart-icon"><ion-icon name="cart"></ion-icon>(0)</NavLink>
+                </div> 
+            </div>
+        )
+    
+    }
 }
 
-export default Navbar;
+const mapStateToProps = state => {
+    return { currentUser: state.userReducer.currentUser}
+}
+
+export default connect(mapStateToProps)(Navbar);
