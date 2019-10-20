14a28d8d63f9d468b9661f1d734704b6f90e13d0
Irma Mejia <ijanettmejia@outlook.com>
2019-10-19 20:19:56 -0400

    Get redux devtools extension

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 
 import App from './App';
 import thunk from 'redux-thunk';
@@ -14,7 +14,10 @@ import * as serviceWorker from './serviceWorker';
 let rootReducer = combineReducers({ itemsReducer, userReducer
     })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
 const rootElement = document.getElementById('root');
 
