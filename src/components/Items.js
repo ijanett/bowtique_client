f563156b22b779d92ac1de79ece8166573c4172f
Irma Mejia <ijanettmejia@outlook.com>
2019-10-19 20:16:30 -0400

    Add filter functionality to Items component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,44 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchItems } from '../actions/fetchItems';
 
-
-class Items extends React.Component {
+class Items extends Component {
     componentDidMount(){
         console.log("MOUNTING")
         this.props.fetchItems()
     }
 
-    render() {
-      console.log(this.props.items)
-        const items = this.props.items.map(item => 
-        
+
+    sizeFilter = (size) => {
+      switch(size) {
+        case 'ALL':
+          return this.props.items
+        case 'SM':
+          return this.props.items.filter(item => item.attributes.size === 'SM');
+        case 'LG':
+          return this.props.items.filter(item => item.attributes.size === 'LG');
+        default:
+          return this.props.items
+      }
+    }
+
+    renderItems = () => {
+      if(this.sizeFilter() && this.props.filterItems) {
+        return this.sizeFilter(this.props.filter).map(item => 
           <div className="col-sm-3" key={item.id}>
               <img src={item.attributes.image} alt={item.attributes.name} className="img-fluid"></img> 
               <h5 align="center">{item.attributes.name}</h5>
+              <h6 align="center">${item.attributes.price}.00</h6>
               <h6 align="center">Size: {item.attributes.size}</h6>
-              <p align="center">Price: ${item.attributes.price}.00</p>
               <button key={item.id} type="submit">ADD TO CART</button>
           </div>
         
         )
-    
-      console.log(this.props.items)
-    return (    
-      <div className="container">
-        <div className="bwtq-filter-title">
-          <h2>ALL BOW TIES</h2>
+      }
+    }
+
+    render() {
+      // console.log(this.props.filterItems)
+      return (    
+        <div className="container">
+          <div className="bwtq-filter-title">
+            <h2>{this.props.filterTitle}</h2>
+          </div>
+          <div className="bwtq-size-desc">
+            <p>SM is better suited for small to medium dogs/cats</p>
+            <p>LG is better suited for medium to large dogs.</p>
+          </div>
+          
+          <div className="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+            <div className="btn-group" role="group" aria-label="First group">
+            Filter by size:
+              <button type="button" onClick={this.props.filterItems} id="ALL BOW TIES" value='ALL' className="btn btn-secondary">ALL</button>
+              <button type="button" onClick={this.props.filterItems} id="SMALL BOW TIES" value='SM' className="btn btn-secondary">SM</button>
+              <button type="button" onClick={this.props.filterItems} id="LARGE BOW TIES" value='LG' className="btn btn-secondary">LG</button>
+            </div>
+          </div>
+
+          <div className="row">
+            {this.renderItems()}
+          </div>
         </div>
-        <div className="bwtq-size-desc">
-          <p>SM is better suited for small to medium dogs/cats</p>
-          <p>LG is better suited for medium to large dogs.</p>
-        </div>
-      <div className="row">
-          {items}
-        </div>
-      </div>
-    );
-  }
+        
+      );
+    }
   
 }
 
@@ -47,7 +73,7 @@ class Items extends React.Component {
 // }
 
 const mapStateToProps = state => {
-  return { items: state.itemsReducer.items }
+  return { currentUser: state.userReducer.currentUser, items: state.itemsReducer.items }
 }
 
 export default connect(mapStateToProps, { fetchItems })(Items); 
