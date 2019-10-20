fe7e3eeb1db33734e2ffaf52174870a6003d3623
Irma Mejia <ijanettmejia@outlook.com>
2019-10-19 20:20:45 -0400

    Add user data to user component

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions/fetchUser';
 
 class User extends React.Component {
-    componentDidMount(){
-    }
     render() {
+        // console.log(this.props.carts)
+        const carts = this.props.carts.map(cart => 
+            cart.filter(cart => cart.attributes.checkout === true))
         
+        const orders = carts.map(orderObj =>
+            orderObj.map(order =>
+                <tr key={order.id}>
+                    <td>{order.id}</td>
+                    <td>{order.attributes.order_date}</td>
+                    <td>{order.attributes.items.length}</td>
+                    <td>{order.attributes.total}</td>
+                </tr>
+            )   
+        )
+        console.log(this.props)
         return (
-            <div>
-                User Account
+            <div className="text-center" id="user-container">
+                {this.props.currentUser.id ? 
+                    <div>
+                        <p>Hello, <strong>{this.props.currentUser.attributes.username}</strong></p>
+                        <h5>My Orders</h5>
+                        <p>{this.props.currentUser.attributes.orders.length} order(s).</p>
+                            <br />
+                            <h6><b>ORDER DETAILS</b></h6>
+                            <table className="table table-borderless">
+                                <thead>
+                                    <tr>
+                                        <th>Order No.</th>
+                                        <th>Order Date</th>
+                                        <th>Items</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {orders}
+                                </tbody>
+                            </table> 
+                    </div> 
+                    : 
+                    <div>Please log in to see your account info.</div>}
             </div>
         )
     }
 }
 
 const mapStateToProps = state => {
-    return { user: state.user}
+    return { currentUser: state.userReducer.currentUser, carts: state.userReducer.carts }
 }
 
-export default connect(mapStateToProps, { fetchUser })(User);
+export default connect(mapStateToProps)(User);
