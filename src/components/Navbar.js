7f38ba4885e43d8bf706a778cc46e9516f42e9bc
Irma Mejia <ijanettmejia@outlook.com>
2019-10-21 14:42:00 -0400

    Add functionality to display cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,23 @@ import brandName from '../brandName.png';
 const Navbar = (props) => {
     // debugger
     return (
-        <div className="navbar">
-            <NavLink to="/" className="navbar-logo"><img src={brandName} alt=""/></NavLink>
-            <div className="navbar-list">
-                <NavLink to="/" activeClassName="chosen">Home</NavLink>
-                <NavLink to="/bowties" activeClassName="chosen">Shop</NavLink>
-                
-                {props.currentUser !== null ? <NavLink to="/account" activeClassName="chosen">My Account</NavLink> : <NavLink to="/login" activeClassName="chosen">Login</NavLink> }
-                
-                <NavLink to="/new-cart" className="cart-icon"><ion-icon name="cart"></ion-icon>{props.currentUser !== null ? `(${props.carts[props.carts.length - 1].attributes.items.length})` : '(0)'}</NavLink>
-            </div> 
-        </div>
+        <header className="nav">
+            <nav className="navbar">
+                <div></div>
+                <NavLink to="/" className="navbar-logo"><img src={brandName} alt=""/></NavLink>
+                <div className="spacer" />
+                <div className="navbar-list">
+                    <ul>
+                        <li><NavLink exact to="/" activeClassName="chosen"><ion-icon name="home"></ion-icon></NavLink></li>
+                        <li><NavLink to="/bowties" activeClassName="chosen"><ion-icon name="bowtie"></ion-icon></NavLink></li>
+                        
+                        {props.currentUser !== null ? <li><NavLink to="/account" activeClassName="chosen"><ion-icon name="person"></ion-icon></NavLink></li> : <li><NavLink to="/login" activeClassName="chosen"><ion-icon name="person"></ion-icon></NavLink></li> }
+                        
+                        <li><div onClick={props.cartToggle} className="cart-icon"><ion-icon name="cart"></ion-icon><span className="badge">{props.currentUser !== null ? props.carts[props.carts.length - 1].attributes.items.length : 0}</span></div></li>
+                    </ul>
+                </div> 
+            </nav>
+        </header>
     )
 }
 
