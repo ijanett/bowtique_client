737c3a3a8ffa9211dc6b32c09ac6f1cc3d805e1f
Irma Mejia <ijanettmejia@outlook.com>
2019-10-21 14:42:30 -0400

    Add functionality to hide cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import CartItem from './CartItem';
 
+const Cart = (props) => {
+    console.log(props)
+    let cartClass = 'side-drawer';
+    if (props.visible) {
+        cartClass = 'side-drawer open';
+    }
 
-class Cart extends React.Component {
-    renderCartItems = () => {
+    return (
+        <div className={cartClass}>
+            <div className="cart-header">
+                <h3><div onClick={props.close} className="close-btn"> > </div>Cart</h3>
+            </div>
+
+            {props.currentUser !== null ? <CartItem />
+            :
+            <div className="cart-login-prompt"><p><Link className="login-link" onClick={props.close} to="/login">Log in</Link> to shop</p></div>}
+        </div>
         
-    }
-
-    
-    render() {
-        return (
-            <div></div>
-        ) 
-    }
+    )
 }
 
-const mapStateToProps = state => {
-    return { currentUser: state.userReducer.currentUser}
-}
-
-export default connect(mapStateToProps)(Cart);
+export default Cart;
