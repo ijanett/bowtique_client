3752a6aaa2ca31dfd46ad1a031049f92ae9ee362
Irma Mejia <ijanettmejia@outlook.com>
2019-10-22 22:42:58 -0400

    Create a function to handle cart item delete button

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { deleteCartItem } from '../actions/fetchUser';
 
 class CartItem extends Component {
+    handleClick = (event) => {
+        event.preventDefault()
+
+        let cart_item = this.props.currentUser.attributes.cart_items.find(cartItem =>
+            (cartItem.cart_id === parseInt(this.props.cart_id, 10) && cartItem.item_id === this.props.id)
+        )
+    
+        this.props.deleteCartItem(cart_item)
+    }
 
     renderCartItem = () => {
         // debugger
@@ -11,7 +21,7 @@ class CartItem extends Component {
                     <div className="item-info">{this.props.name}: {this.props.size}
                     <br/><b>QTY:</b> {this.props.quantity}
                     <br/>${this.props.total}.00</div>
-                    <div className="delete-btn">
+                    <div onClick={this.handleClick} className="delete-btn">
                         <ion-icon data-cart-id={this.props.cart_id} data-item-id={this.props.id} name="close-circle-outline"></ion-icon>
                     </div>
                     </div>
@@ -27,4 +37,4 @@ class CartItem extends Component {
     }
 }
 
-export default CartItem;
+export default connect(null, {deleteCartItem})(CartItem);
