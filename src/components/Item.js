9b1b85bd82e560be973766b80229ea5969d7056b
Irma Mejia <ijanettmejia@outlook.com>
2019-10-28 19:50:23 -0400

    Add dataset to button

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,6 +5,27 @@ import swal from 'sweetalert';
 
 class Item extends Component {
 
+  // PROJECT ASSESSMENT LIVE CODING:
+  // state = {
+  //   liked: 0
+  // }
+
+  // likeItem = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       liked: prevState.liked + 1
+  //     }
+  //   })
+  // }
+
+  // iconName = () => {
+  //   if(this.state.liked > 0) {
+  //     return "heart"
+  //   } else {
+  //     return "heart-empty"
+  //   }
+  // }
+
     handleClick = (event) => {
       event.preventDefault()
       let user = this.props.currentUser
@@ -21,7 +42,7 @@ class Item extends Component {
       } else {
         let carts = user.relationships.carts.data  
         let cartId = carts[carts.length - 1].id
-        let itemId = event.target.id
+        let itemId = event.target.dataset.itemId
         let cartItem = {
           cart_id: cartId,
           item_id: itemId
@@ -32,11 +53,13 @@ class Item extends Component {
 
     renderItem = () => {
         return  <div className="col-sm-3" key={this.props.key}>
-                    <img src={this.props.img} alt={this.props.name} className="img-fluid"></img> 
+                    <img src={this.props.img} alt={this.props.name} className="img-fluid"></img>
                     <h5 align="center">{this.props.name}</h5>
                     <h6 align="center">${this.props.price}.00</h6>
                     <h6 align="center">Size: {this.props.size}</h6>
-                    <button id={this.props.id} onClick={this.handleClick} type="submit">ADD TO CART</button>
+                    {/*LIVE CODING*/}
+                    {/*<div className="text-center"><ion-icon onClick={this.likeItem} name={this.iconName()}></ion-icon> {this.state.liked > 0 ? this.state.liked : ''}</div>*/}
+                    <button data-item-id={this.props.id} onClick={this.handleClick} type="submit">ADD TO CART</button>
                 </div>
       
     }
